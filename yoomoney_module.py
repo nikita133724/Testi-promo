22f5040a02b231069a3f88162895f56516c595ef
nikita133724 <33579082+nikita133724@users.noreply.github.com>
2025-12-30 12:22:30 +0600

    Update yoomoney_module.py

--- yoomoney_module.py
+++ yoomoney_module.py
@@ -5,7 +5,20 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 CLIENT_ID = "F11DB8BE3A35360A2005C5F1542E842FB12BF7ABC8F78ED9BF2C02AC4C79F2A9"
 CLIENT_SECRET = "75344E66298F18E1565721F87032CC4B180389C5A82A710FE7EF249C01E3890F8F40D657BDC51FF05726A7CA15A605777306B43E9E79EC4130A6E76504978F3D"
 SUCCESS_REDIRECT_URI = "https://tg-bot-test-gkbp.onrender.com/payment/success"
+YOOMONEY_WALLET = "4100117872411525"  
 
+def create_payment_link(chat_id: int, amount: int) -> str:
+    comment = f"user_{chat_id}"
+    url = (
+        f"https://yoomoney.ru/quickpay/confirm.xml"
+        f"?receiver={YOOMONEY_WALLET}"
+        f"&quickpay-form=shop"
+        f"&targets={comment}"
+        f"&sum={amount}"
+        f"&currency=643"
+        f"&successURL={SUCCESS_REDIRECT_URI}"
+    )
+    return url
 async def get_access_token() -> str:
     """Получение access_token через OAuth2 client credentials"""
     async with aiohttp.ClientSession() as session:
@@ -36,7 +49,6 @@ async def send_payment_link(bot, chat_id: int, amount: int):
     """
     Генерация ссылки и отправка пользователю через Telegram
     """
-    token = await get_access_token()
-    payment_url = await create_payment_link(chat_id, amount, token)
+    payment_url = create_payment_link(chat_id, amount)
     keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Оплатить подписку", url=payment_url)]])
     await bot.send_message(chat_id, "Нажмите для оплаты:", reply_markup=keyboard)
