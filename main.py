17df7b95c38d5a884b70bfeea9a92e83a2f8e649
nikita133724 <33579082+nikita133724@users.noreply.github.com>
2025-12-31 21:30:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from datetime import timezone, timedelta, datetime
 from zoneinfo import ZoneInfo
 # -----------------------
 # Telegram и RAM_DATA
-from telegram_client import client, init_channel_ids
+from telegram_client import client
 from telegram_bot import app as tg_app, bot, load_chatids, build_reply_keyboard, RAM_DATA, _save_to_redis_partial, send_message_to_user, ReplyKeyboardMarkup
 from refresh_tokens import token_refresher_loop
 from access_control import subscription_watcher, generate_key
@@ -519,7 +519,6 @@ async def start_telegram():
     await tg_app.initialize()
     await tg_app.start()
     await client.start()
-    await init_channel_ids()
     print("Telethon клиент запущен.")
     await tg_app.updater.start_polling()
     
