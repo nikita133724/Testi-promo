6905a09e86ba75ea716f5c074e618148b5f296aa
nikita133724 <33579082+nikita133724@users.noreply.github.com>
2026-01-07 18:19:14 +0600

    Create orders_store.py

---
+++ orders_store.py
@@ -0,0 +1,33 @@
+import json
+from datetime import datetime, timezone
+from redis_client import r
+
+ORDERS_KEY = "GLOBAL_ORDERS"
+COUNTER_KEY = "GLOBAL_ORDER_COUNTER"
+
+ORDERS = {}
+
+
+def load_orders():
+    global ORDERS
+    raw = r.hgetall(ORDERS_KEY)
+    for k, v in raw.items():
+        ORDERS[int(k)] = json.loads(v)
+
+
+def save_order(order_id, data):
+    ORDERS[order_id] = data
+    r.hset(ORDERS_KEY, order_id, json.dumps(data))
+
+
+def next_order_id():
+    new_id = r.incr(COUNTER_KEY)
+    return int(new_id)
+
+
+def get_user_orders(chat_id):
+    return [(oid, o) for oid, o in ORDERS.items() if o["chat_id"] == chat_id]
+
+
+def get_order(order_id):
+    return ORDERS.get(order_id)
